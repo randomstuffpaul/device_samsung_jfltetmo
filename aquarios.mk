425f08f9344819daf63a0a9a5fd048faecee33e7
Paul Keith <javelinanddart@bestas.gr>
2017-03-22 21:09:47 -0500

    Aqua-ify?
    Signed-off-by: Paul Keith <javelinanddart@bestas.gr>

--- aquarios.mk
+++ aquarios.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/samsung/jfltetmo/full_jfltetmo.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Aquarios stuff.
+$(call inherit-product, vendor/aquarios/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
   PRODUCT_NAME=jfltetmo \
@@ -9,6 +9,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
   BUILD_FINGERPRINT="samsung/jfltetmo/jfltetmo:4.4.4/KTU84P/M919UVSFQA1:user/release-keys" \
   PRIVATE_BUILD_DESC="jfltetmo-user 4.4.4 KTU84P M919UVSFQA1 release-keys"
 
-PRODUCT_NAME := lineage_jfltetmo
+PRODUCT_NAME := aquarios_jfltetmo
 PRODUCT_DEVICE := jfltetmo
 
